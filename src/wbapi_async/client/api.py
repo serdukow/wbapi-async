30327b50e73734d0740c2fc47c8c1a663e4a6d79
Andrei Serdiukov <98782177+serdukow@users.noreply.github.com>
2026-03-19 11:29:15 +0300

    feat: add WbAPI client class with async context manager support

---
+++ src/wbapi_async/client/api.py
@@ -0,0 +1,61 @@
+from typing import Any
+
+from ..client.session.base import BaseSession
+from ..methods.base import WbMethod
+from ..methods.connection_check import ConnectionCheck as ConnectionCheckMethod
+from ..methods.get_products_with_prices import GetProductsWithPrices
+from ..types import ConnectionCheck, ProductWithPrice
+from ..utils.token import validate_token
+
+
+class WbAPI:
+    def __init__(self, token: str, session: BaseSession | None = None, **kwargs: Any) -> None:
+        """
+        WbAPI class.
+
+        Attributes:
+
+            token: Access token
+
+            Source: https://dev.wildberries.ru/en/docs/openapi/api-information#tag/Authorization/How-to-create-a-personal-access-base-or-test-token
+        """
+        validate_token(token)
+        if session is None:
+            read_timeout = kwargs.get("read_timeout", 60)
+            base = kwargs.get("base", "wildberries.ru")
+            session = BaseSession(
+                base=base,
+                timeout=read_timeout,
+            )
+
+        self._token = token
+        self.session = session
+
+    async def __aenter__(self) -> "WbAPI":
+        return self
+
+    async def __aexit__(self, exc_type: Any, _exc: Any, _tb: Any) -> None:
+        await self.session.close()
+
+    async def __call__(self, method: WbMethod) -> Any:
+        return await method.emit(self)
+
+    async def connection_check(self) -> ConnectionCheck:
+        call = ConnectionCheckMethod()
+        return await self(call)
+
+    async def get_products_with_prices(
+        self, limit: int | None = 1000, offset: int | None = 0, filter_nm_id: int | None = None
+    ) -> list[ProductWithPrice]:
+        """
+        Returns product data.
+
+        Source: https://dev.wildberries.ru/en/docs/openapi/work-with-products#tag/Prices-and-Discounts/paths/~1api~1v2~1list~1goods~1filter/get
+
+        :param limit: Number of elements per page (pagination)
+        :param offset: How many results to skip
+        :param filter_nm_id: WB article for search
+        :return: :class:`ProductWithPrice`: Product data
+        """
+        call = GetProductsWithPrices(limit=limit, offset=offset, filter_nm_id=filter_nm_id)
+        return await self(call)
